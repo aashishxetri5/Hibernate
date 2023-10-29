eeedd08c4e6bfe866a6329a6542c037ffa71152d
Aashish Katwal <aashiskutwal5@gmail.com>
2023-10-29 22:05:33 +0545

    HQL delete with dynamic parameter setting

--- Hibernate/src/main/java/com/hql/HQLExample.java
+++ Hibernate/src/main/java/com/hql/HQLExample.java
@@ -4,18 +4,22 @@ import java.util.List;
 
 import javax.persistence.TypedQuery;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import com.learning.entities.Student;
 
 public class HQLExample {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws HibernateException{
 		SessionFactory factory = new Configuration().configure().buildSessionFactory();
 
 		Session session = factory.openSession();
+		Transaction tx = session.beginTransaction();
 
 //		TypedQuery<Student> query = session.createQuery("from Student", Student.class); //HQL => "from Student"
 		TypedQuery<Student> query = session.createQuery("from Student where id= :stdId", Student.class);
@@ -27,6 +31,20 @@ public class HQLExample {
 		for (Student student : allStudents) {
 			System.out.println(student);
 		}
+		
+		System.out.println("----------------------------------------");
+		
+		@SuppressWarnings("deprecation")
+		Query<Student> deleteQuery = session.createQuery("delete from Student where id= :studentId");
+		deleteQuery.setParameter("studentId", 211802);
+		
+		int r = deleteQuery.executeUpdate();
+		System.out.println("Deleted " + r);
+		
+		
+		tx.commit();
+		session.close();
+		factory.close();
 
 	}
 
